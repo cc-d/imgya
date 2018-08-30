fe606f529f3aa6426acdc85de3921aa0f49f44ae
Ubuntu <ubuntu@ip-172-31-51-65.ec2.internal>
2018-08-30 22:30:55 +0000

    config

--- imgya_website/__init__.py
+++ imgya_website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, session, g, render_template
 from pprint import pprint
 
 app = Flask(__name__)
-app.config.from_object('prod_config')
+app.config.from_object('dev_config')
 
 @app.route('/')
 def hello_world():
